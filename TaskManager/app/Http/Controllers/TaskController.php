48f8df0354f8be0cb1c41e190cece848067e7e77
RafaelYuta <rafael.nakayama05@edu.unifil.br>
2023-07-03 15:52:39 -0300

    TaskManager e Video
    Pasta do TaskManager e video mostrando o funcionamento da tabela utilizando o postman

---
+++ TaskManager/app/Http/Controllers/TaskController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json($tasks);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'Titulo' => 'required',
+            'Descricao' => 'required',
+            'Status' => 'required',
+        ]);
+        return Task::create($request->all());
+
+    }
+
+    public function show(Task $task)
+    {
+        return response()->json($task);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $request->validate([
+            'Titulo' => 'required',
+            'Descricao' => 'required',
+            'Status' => 'required',
+        ]);
+        $task->update($request->all());
+        return response()->json($task);
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json(null, 204);
+    }
+}
